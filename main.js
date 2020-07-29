cb5779931f05739619bb39645b37f168edd569c4
unknown <xvinh.ir@gmail.com>
2020-07-29 07:45:30 +0700

    beautified

--- main.js
+++ main.js
@@ -4,11 +4,11 @@
 //3. 없으면 챈스가 깍이고 행맨을 그림
 //4. 챈스 0이면 끝남
 //5. 글씨 맞추면 추카
-let alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let alphabets = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let selectedList = [];
 let context, myStickman;
-let buttonHTML = alphabets.map((item) => `<button class="btn btn-warning" id="${item}" onclick="selectAlphabet('${item}')">${item.toUpperCase()}</button>`).join('');
-document.getElementById('buttonArea').innerHTML = buttonHTML;
+let buttonHTML = alphabets.map((item) => `<button class="btn btn-warning" id="${item}" onclick="selectAlphabet('${item}')">${item.toUpperCase()}</button>`).join("");
+document.getElementById("buttonArea").innerHTML = buttonHTML;
 function drawHackman() {
 	drawArray[count]();
 }
@@ -50,33 +50,33 @@ leftLeg = function () {
 };
 
 head = function () {
-	myStickman = document.getElementById('canvas');
-	context = myStickman.getContext('2d');
+	myStickman = document.getElementById("canvas");
+	context = myStickman.getContext("2d");
 	context.beginPath();
 	context.arc(60, 25, 10, 0, Math.PI * 2, true);
 	context.stroke();
 };
 let round = 0;
 let wordList = [
-	'alien',
-	'avengers',
-	'titanic',
-	'matrix',
-	'memento',
-	'pianist',
-	'aquaman',
-	'annabelle',
-	'deadpool',
-	'jumanji',
-	'spiderman',
-	'frozen',
-	'shrek',
-	'notebook',
-	'avatar',
-	'mulan',
-	'godfather',
-	'leon',
-	'psycho',
+	"alien",
+	"avengers",
+	"titanic",
+	"matrix",
+	"memento",
+	"pianist",
+	"aquaman",
+	"annabelle",
+	"deadpool",
+	"jumanji",
+	"spiderman",
+	"frozen",
+	"shrek",
+	"notebook",
+	"avatar",
+	"mulan",
+	"godfather",
+	"leon",
+	"psycho"
 ];
 let word = wordList[round];
 let answer = [];
@@ -85,10 +85,10 @@ let correct = 0;
 let count = 10;
 let gameover = false;
 function render() {
-	document.getElementById('alphabet').innerHTML = word
-		.split('')
-		.map((letter) => `<div class="letter"></div>`)
-		.join('');
+	document.getElementById("alphabet").innerHTML = word
+		.split("")
+		.map((letter) => `<div class="letter"><div class="pot"><img src="img/pot2.png"/></div></div>`)
+		.join("");
 }
 
 function selectAlphabet(char) {
@@ -98,28 +98,32 @@ function selectAlphabet(char) {
 	let correct = 0;
 	document.getElementById(char).disabled = true;
 	selectedList.push(char);
-	let wordArray = word.split('');
+	let wordArray = word.split("");
 	if (wordArray.includes(char)) {
 		answer.push(char);
-		document.getElementById('alphabet').innerHTML = wordArray
-			.map((letter) => {
+		document.getElementById("alphabet").innerHTML = wordArray
+			.map((letter, idx) => {
 				if (answer.includes(letter)) {
 					correct++;
-					return `<div class="letter">${letter}</div>`;
+					return `<div id="${idx}" class="letter grow"><div class="flower"><div class="letter-on-top">${letter.toUpperCase()}</div></div><div class="pot"><img src="img/pot2.png"></div></div>`;
 				}
-				return `<div class="letter"></div>`;
+				return `<div class="letter"><div class="pot"><img src="img/pot2.png"/></div></div>`;
 			})
-			.join('');
+			.join("");
 		console.log(correct, wordArray.length);
 		if (correct == wordArray.length) {
-			document.getElementById('resultArea').innerHTML = 'You Win!!';
+			document.getElementById("resultArea").innerHTML = "You Win!!";
+			document.getElementById("teacher").src = "img/happyteacher.png";
+			document.getElementById("resultArea").classList.add("green");
 		}
 	} else {
 		count--;
 
 		drawHackman();
 		if (count <= 0) {
-			document.getElementById('resultArea').innerHTML = 'GAME OVER';
+			document.getElementById("resultArea").innerHTML = "GAME OVER";
+			document.getElementById("teacher").src = "img/sadteacher.png";
+			document.getElementById("resultArea").classList.add("red");
 			gameover = true;
 			return;
 		}
@@ -134,7 +138,7 @@ function reset() {
 	correct = 0;
 	count = 10;
 	selectedList.map((item) => (document.getElementById(`${item}`).disabled = false));
-	document.getElementById('resultArea').innerHTML = '';
+	document.getElementById("resultArea").innerHTML = "";
 	context.clearRect(0, 0, myStickman.width, myStickman.height);
 	canvas();
 	gameover = false;
@@ -146,10 +150,10 @@ draw = function ($pathFromx, $pathFromy, $pathTox, $pathToy) {
 	context.stroke();
 };
 canvas = function () {
-	myStickman = document.getElementById('canvas');
-	context = myStickman.getContext('2d');
+	myStickman = document.getElementById("canvas");
+	context = myStickman.getContext("2d");
 	context.beginPath();
-	context.strokeStyle = '#4287f5';
+	context.strokeStyle = "#4287f5";
 	context.lineWidth = 2;
 };
 canvas();
